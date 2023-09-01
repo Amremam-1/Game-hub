1b64916165fc18af6c18cdc8fafc784994fdd2bf
amr emam <emama2337@gmail.com>
2023-09-01 20:03:28 +0300

    Displaying platform icons

--- src/components/GameGard.tsx
+++ src/components/GameGard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/UseGames";
+import PlatFormIconList from "./PlatFormIconList";
 
 interface props {
   game: Game;
@@ -12,6 +13,9 @@ const GameGard = ({ game }: props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatFormIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -3,11 +3,19 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
 
+export interface platform{
+  id : number;
+  name : string;
+  slug : string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform : platform}[];
   }
+
   interface FatchGameResponse {
     count: number;
     results: Game[];
