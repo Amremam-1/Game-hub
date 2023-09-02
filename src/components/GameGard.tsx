3d457ecbc0e75670be8c335102d6743711bafed1
amr emam <emama2337@gmail.com>
2023-09-02 21:18:00 +0300

    get optimized images

--- src/components/GameGard.tsx
+++ src/components/GameGard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import React from "react";
 import { Game } from "../hooks/UseGames";
-import CriticScore from "./criticScore";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
 import PlatFormIconList from "./PlatFormIconList";
 
 interface props {
@@ -11,7 +11,7 @@ interface props {
 const GameGard = ({ game }: props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
