0a8b06f12b1b7111aa35cc56984003c956ebfdff
amr emam <emama2337@gmail.com>
2023-09-20 17:32:44 +0300

    fatching data with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,7 +3,6 @@ import UseGames from "../hooks/UseGames";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameGard from "./GameGard";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface props {
@@ -14,7 +13,7 @@ const GameGrid = ({ gameQuery }: props) => {
   const { data, error, isLoading } = UseGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -27,7 +26,7 @@ const GameGrid = ({ gameQuery }: props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameGard game={game} />
         </GameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-  export default axios.create({
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "28257b631bf641248a547599ebb36501",
