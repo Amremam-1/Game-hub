0d862135db4893a2ae7e6569992c124396fcdcdb
amr emam <emama2337@gmail.com>
2023-10-05 23:57:18 +0300

    Build Game ScreenShots

---
+++ src/hooks/useScreenshot.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { screenshots } from "../entities/Screenshot";
+import APIClient from "../services/api-client";
+
+const useScreenshots = (gameId: number) => {
+  const apiClinet = new APIClient<screenshots>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClinet.getAll,
+  });
+};
+
+export default useScreenshots;
