d7a8daa3392c14939c90ae90d2ad0bbb1d604621
Amr Imam <emama2337@gmail.com>
2023-11-27 02:15:09 +0200

    last change

--- src/components/GameGard.tsx
+++ src/components/GameGard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import Game from "../entities/Game";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
